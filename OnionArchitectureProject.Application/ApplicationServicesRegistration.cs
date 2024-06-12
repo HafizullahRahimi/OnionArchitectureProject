61988b92739a18ff500d962951129957a0ee2492
Hafiz <HafizullahRahimi@outlooo.com>
2024-06-12 15:26:51 +0200

    Add project files.

---
+++ OnionArchitectureProject.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace OnionArchitectureProject.Application;
+public static class ApplicationServicesRegistration
+{
+    public static void AddApplicationLayer(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+    }
+}
