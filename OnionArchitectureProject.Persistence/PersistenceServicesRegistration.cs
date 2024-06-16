48f0ceb3f28b483a09c9df1dca6b6e8fe3c54027
Hafiz <HafizullahRahimi@outlooo.com>
2024-06-16 15:10:58 +0200

    Add Api
    OnionArchitectureProject.Api

--- OnionArchitectureProject.Persistence/PersistenceServicesRegistration.cs
+++ OnionArchitectureProject.Persistence/PersistenceServicesRegistration.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using OnionArchitectureProject.Persistence;
 
-namespace OnionArchitectureProject.Application;
+namespace OnionArchitectureProject.Persistence;
 public static class PersistenceServicesRegistration
 {
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
@@ -11,6 +10,7 @@ public static class PersistenceServicesRegistration
         var connectionString = configuration
             .GetConnectionString("DefaultConnection") ??
             throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
         services.AddDbContextFactory<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
         #region Repositories
