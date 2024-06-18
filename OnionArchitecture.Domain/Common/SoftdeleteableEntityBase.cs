663c8532a25c63d92f56aaa25e4fabf809b2a576
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-06-18 18:28:31 +0200

    Add Repositories

--- OnionArchitecture.Domain/Common/SoftdeleteableEntityBase.cs
+++ OnionArchitecture.Domain/Common/SoftdeleteableEntityBase.cs
@@ -1,7 +1,7 @@
 ﻿using OnionArchitecture.Domain.Common.Interfaces;
 
 namespace OnionArchitecture.Domain.Common;
-public class SoftdeleteableEntityBase<TId> : EntityBase<TId>, ISoftDeleted where TId : notnull
+public abstract class SoftdeleteableEntityBase<TId> : EntityBase<TId>, ISoftDeleted where TId : notnull
 {
     public bool IsDeleted { get; set; } = false;
     public DateTime? DeletedDateUTC { get; set; }
---
+++ OnionArchitectureProject.Application/Contracts/Persistence/IRepositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace OnionArchitectureProject.Application.Contracts.Persistence.IRepositories;
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(Guid id);
+    Task<List<TEntity>> GetAllAsync();
+    Task<bool> ExistAsync(Guid id);
+    Task<TEntity> Add(TEntity entity);
+    void Update(TEntity entity);
+    void Remove(TEntity entity);
+    void RemoveById(Guid id);
+    Task SaveChangesAsync();
+}
