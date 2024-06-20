65c32153b0fd482353e15b8fc5916c7e4a0da43c
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-06-20 12:51:29 +0200

    Set EditableEntityBase to abstract

--- OnionArchitecture.Domain/Common/EditableEntityBase.cs
+++ OnionArchitecture.Domain/Common/EditableEntityBase.cs
@@ -1,7 +1,7 @@
 ﻿using OnionArchitecture.Domain.Common.Interfaces;
 
 namespace OnionArchitecture.Domain.Common;
-public class EditableEntityBase<TId> : EntityBase<TId>, ICreated, IModified where TId : notnull
+public abstract class EditableEntityBase<TId> : EntityBase<TId>, ICreated, IModified where TId : notnull
 {
     public DateTime CreatedDateUTC { get; set; }
     public string CreatedBy { get; set; } = string.Empty;
