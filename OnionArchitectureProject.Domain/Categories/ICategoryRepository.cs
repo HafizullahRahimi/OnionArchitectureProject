14b392e9f28f26fe8bee1d221740da2c2c13efa8
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-07-05 13:46:15 +0200

    Clean up

--- OnionArchitectureProject.Domain/Categories/ICategoryRepository.cs
+++ OnionArchitectureProject.Domain/Categories/ICategoryRepository.cs
@@ -1,5 +1,4 @@
 ﻿using OnionArchitectureProject.Domain.Common.Repositories;
-using OnionArchitectureProject.Domain.Categories;
 
 namespace OnionArchitectureProject.Domain.Categories;
 public interface ICategoryRepository : IRepository<Category> { }
