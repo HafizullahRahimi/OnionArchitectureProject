d5b6a9864c5d776a69c06a2af0817b99a1704ca6
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-07-05 14:29:16 +0200

    Add ApplicationDbContext and Configurations
    Add ApplicationDbContext , CategoryConfiguration  and ProductConfiguration

---
+++ OnionArchitectureProject.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArchitectureProject.Domain.Categories;
+using OnionArchitectureProject.Domain.Products;
+
+namespace OnionArchitectureProject.Persistence;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
---
+++ OnionArchitectureProject.Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnionArchitectureProject.Domain.Products;
+
+namespace OnionArchitectureProject.Persistence.Configurations;
+internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder
+            .Property(p => p.Price)
+            .HasPrecision(18, 2);
+
+        builder
+            .HasOne(p => p.Category)
+            .WithMany(p => p.Products)
+            .HasForeignKey(b => b.CategoryId);
+
+        builder
+            .HasIndex(_ => _.IsDeleted)
+            .HasFilter("IsDeleted = 0");
+
+        builder
+            .HasQueryFilter(_ => !_.IsDeleted);
+    }
+}
