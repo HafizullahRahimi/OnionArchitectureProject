5eb4d574427379af7c7a0e22d117fa0dffd92066
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-07-05 13:36:02 +0200

    Add categories and products
    Entity and IRepository

---
+++ OnionArchitectureProject.Domain/Products/Product.cs
@@ -0,0 +1,13 @@
+﻿using OnionArchitectureProject.Domain.Common.BaseEntities;
+using OnionArchitectureProject.Domain.Categories;
+
+namespace OnionArchitectureProject.Domain.Products;
+public class Product : EditableEntityBase<Guid>
+{
+    public string Name { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; }
+}
