51d80365257c1c5f2d273e3c208963a20491e458
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-07-05 17:27:32 +0200

    Add ApplicationServicesRegistration

---
+++ OnionArchitectureProject.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace OnionArchitectureProject.Application;
+public static class ApplicationServicesRegistration
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+    }
+}
