56461ce4ff558b5023e2cc71e18e04afd1c99505
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-07-05 14:53:26 +0200

    Add HttpContextAccessorExtension

---
+++ OnionArchitectureProject.Persistence/Extensions/HttpContextAccessorExtension.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace OnionArchitectureProject.Persistence.Extensions;
+internal static class HttpContextAccessorExtension
+{
+    public static string GetClaimsByType(this IHttpContextAccessor httpContextAccessor, string claimsType)
+    {
+        return "User id";
+    }
+}
