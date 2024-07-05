33f8ca0ebf6b4f11b6ff6d7b8b09ec1aa7e2f74e
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-07-05 14:48:11 +0200

    Add PersistenceServicesRegistration and Interceptors
    Add PersistenceServicesRegistration , CreatedInterceptor , ModifiedInterceptor and SoftDeletedInterceptor

---
+++ OnionArchitectureProject.Persistence/Interceptors/CreatedInterceptor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using OnionArchitectureProject.Domain.Common.BaseEntities;
+
+namespace OnionArchitectureProject.Persistence.Interceptors;
+public class CreatedInterceptor : SaveChangesInterceptor
+{
+    private readonly IHttpContextAccessor httpContextAccessor;
+
+    public CreatedInterceptor(IHttpContextAccessor httpContextAccessor)
+    {
+        this.httpContextAccessor = httpContextAccessor;
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+
+        if (eventData.Context is null)
+        {
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        IEnumerable<EntityEntry<ICreated>> entries =
+              eventData
+              .Context
+              .ChangeTracker.Entries<ICreated>()
+              .Where(_ => _.State == Microsoft.EntityFrameworkCore.EntityState.Added);
+
+        //var currentUserId = httpContextAccessor.GetClaimsByType("uid");
+        var currentUserId = "Created by user id";
+
+        foreach (EntityEntry<ICreated> softDeletable in entries)
+        {
+            softDeletable.State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            softDeletable.Entity.CreatedBy = currentUserId;
+            softDeletable.Entity.CreatedAt = DateTime.Now;
+        }
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+}
---
+++ OnionArchitectureProject.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnionArchitectureProject.Persistence.Interceptors;
+
+namespace OnionArchitectureProject.Persistence;
+public static class PersistenceServicesRegistration
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration
+            .GetConnectionString("DefaultConnection") ??
+            throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+        services.AddSingleton<SoftDeletedInterceptor>();
+        services.AddSingleton<CreatedInterceptor>();
+        services.AddSingleton<ModifiedInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>(
+            (sp, option) => option
+            .UseSqlServer(connectionString)
+            .AddInterceptors(sp.GetRequiredService<SoftDeletedInterceptor>())
+            .AddInterceptors(sp.GetRequiredService<CreatedInterceptor>())
+            .AddInterceptors(sp.GetRequiredService<ModifiedInterceptor>())
+            );
+
+        //services.AddDbContextFactory<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+        #region Repositories
+        #endregion
+
+        return services;
+    }
+}
