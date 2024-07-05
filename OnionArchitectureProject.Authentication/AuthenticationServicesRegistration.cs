92e32c6a100e1081e8ca4fb8b50e38dfca89d7bc
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-07-05 18:26:36 +0200

    Add AuthenticationDbContext
    Add AuthenticationDbContext and AuthenticationServicesRegistration

---
+++ OnionArchitectureProject.Authentication/AuthenticationServicesRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnionArchitectureProject.Authentication.Models;
+
+namespace OnionArchitectureProject.Authentication;
+public static class AuthenticationServicesRegistration
+{
+    public static IServiceCollection AddAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration
+            .GetConnectionString("DefaultConnection") ??
+            throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+        services.AddDbContext<AuthenticationDbContext>(options =>
+            options.UseSqlServer(
+                connectionString,
+                b => b.MigrationsAssembly(typeof(AuthenticationDbContext).Assembly.FullName)
+            ));
+        services.AddIdentityCore<ApplicationUser>()
+            .AddEntityFrameworkStores<AuthenticationDbContext>();
+
+        #region Servises
+        #endregion
+
+        return services;
+    }
+}
