06acf83f961b27d269f5d96e1b81931f2c24ac83
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-07-05 17:29:45 +0200

    Add services
    Add CategoryService and ProductService

---
+++ OnionArchitectureProject.Application/Services/CategoryService/Models/CategoryDto.cs
@@ -0,0 +1,7 @@
+﻿using OnionArchitectureProject.Domain.Common.BaseEntities;
+
+namespace OnionArchitectureProject.Application.Services.CategoryService.Models;
+public class CategoryDto : EditableEntityBase<Guid>
+{
+    public string Name { get; set; } = string.Empty;
+}
---
+++ OnionArchitectureProject.Application/Services/CategoryService/Profiles/CategoryProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using OnionArchitectureProject.Domain.Categories;
+using OnionArchitectureProject.Application.Services.CategoryService.Models;
+
+namespace OnionArchitectureProject.Application.Profiles;
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryDto>().ReverseMap();
+    }
+}
