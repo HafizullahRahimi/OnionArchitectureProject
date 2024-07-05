a33ff66e7ed94ffe1566d37725aea1087ba55276
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-07-05 18:26:36 +0200

    Add AuthenticationDbContext
    Add AuthenticationDbContext and AuthenticationServicesRegistration

---
+++ OnionArchitectureProject.Authentication/AuthenticationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnionArchitectureProject.Authentication.Models;
+
+namespace OnionArchitectureProject.Authentication;
+public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(AuthenticationDbContext).Assembly);
+    }
+}
