7d473a8392e55edfb1f852653fa7a765b4a5ece1
Hafizullah Rahimi <HafizullahRahimi@outlook.com>
2024-07-08 15:39:17 +0200

    refactor : delete identity tbls seed data and remove user properies

--- OnionArchitectureProject.Authentication/Configurations/RoleConfiguration.cs
+++ OnionArchitectureProject.Authentication/Configurations/RoleConfiguration.cs
@@ -7,19 +7,19 @@ public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
 {
     public void Configure(EntityTypeBuilder<IdentityRole> builder)
     {
-        builder.HasData(
-            new IdentityRole
-            {
-                Id = "9845f909-799c-45fd-9158-58c1336ffddc",
-                Name = "User",
-                NormalizedName = "USER"
-            },
-            new IdentityRole
-            {
-                Id = "cb275765-1cac-4652-a03f-f8871dd575d1",
-                Name = "Admin",
-                NormalizedName = "ADMIN"
-            }
-        );
+        //builder.HasData(
+        //    new IdentityRole
+        //    {
+        //        Id = "9845f909-799c-45fd-9158-58c1336ffddc",
+        //        Name = "User",
+        //        NormalizedName = "USER"
+        //    },
+        //    new IdentityRole
+        //    {
+        //        Id = "cb275765-1cac-4652-a03f-f8871dd575d1",
+        //        Name = "Admin",
+        //        NormalizedName = "ADMIN"
+        //    }
+        //);
     }
 }
--- OnionArchitectureProject.Authentication/Configurations/UserConfiguration.cs
+++ OnionArchitectureProject.Authentication/Configurations/UserConfiguration.cs
@@ -8,33 +8,33 @@ public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
 {
     public void Configure(EntityTypeBuilder<ApplicationUser> builder)
     {
-        var hasher = new PasswordHasher<ApplicationUser>();
+        //var hasher = new PasswordHasher<ApplicationUser>();
 
-        builder.HasData(
-            new ApplicationUser
-            {
-                Id = "05446344-f9cc-4566-bd2c-36791b4e28ed",
-                Email = "admin@localhost.com",
-                NormalizedEmail = "ADMIN@LOCALHOST.COM",
-                FirstName = "Admin",
-                LastName = "System",
-                UserName = "Admin10",
-                NormalizedUserName = "ADMIN10",
-                PasswordHash = hasher.HashPassword(null, "P@ssword1"),
-                EmailConfirmed = true,
-            },
-            new ApplicationUser
-            {
-                Id = "2ec9f480-7288-4d0f-a1cd-53cc89968b45",
-                Email = "user@localhost.com",
-                NormalizedEmail = "USER@LOCALHOST.COM",
-                FirstName = "User",
-                LastName = "System",
-                UserName = "User20",
-                NormalizedUserName = "USER20",
-                PasswordHash = hasher.HashPassword(null, "P@ssword1"),
-                EmailConfirmed = true,
-            }
-        );
+        //builder.HasData(
+        //    new ApplicationUser
+        //    {
+        //        Id = "05446344-f9cc-4566-bd2c-36791b4e28ed",
+        //        Email = "admin@localhost.com",
+        //        NormalizedEmail = "ADMIN@LOCALHOST.COM",
+        //        FirstName = "Admin",
+        //        LastName = "System",
+        //        UserName = "Admin10",
+        //        NormalizedUserName = "ADMIN10",
+        //        PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+        //        EmailConfirmed = true,
+        //    },
+        //    new ApplicationUser
+        //    {
+        //        Id = "2ec9f480-7288-4d0f-a1cd-53cc89968b45",
+        //        Email = "user@localhost.com",
+        //        NormalizedEmail = "USER@LOCALHOST.COM",
+        //        FirstName = "User",
+        //        LastName = "System",
+        //        UserName = "User20",
+        //        NormalizedUserName = "USER20",
+        //        PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+        //        EmailConfirmed = true,
+        //    }
+        //);
     }
 }
