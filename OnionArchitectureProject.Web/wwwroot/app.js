12f1a6dccd766bcd01fbf4793a78d694c5dbbca0
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2024-11-08 17:52:01 +0100

    feat: add modal-base and modal-sing-out

---
+++ OnionArchitectureProject.Web/wwwroot/app.js
@@ -0,0 +1,10 @@
+﻿function OpenModal(modalId) {
+    var myModal = new bootstrap.Modal(document.getElementById(modalId));
+    myModal.show();
+}
+
+function CloseModal(modalId) {
+    var myModalEl = document.getElementById(modalId);
+    var modal = bootstrap.Modal.getInstance(myModalEl); // Returns a Bootstrap modal instance
+    modal.hide();
+}
