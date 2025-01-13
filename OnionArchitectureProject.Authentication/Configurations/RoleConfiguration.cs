38cacb7b9257264b5c1c335cecbf9a030f10d652
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2025-01-13 16:33:11 +0100

    refactor: commit role, user and useRole configurations

--- OnionArchitectureProject.Authentication/Configurations/RoleConfiguration.cs
+++ OnionArchitectureProject.Authentication/Configurations/RoleConfiguration.cs
@@ -1,26 +1,26 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using OnionArchitectureProject.Domain.Authentication.ApplicationRole;
+﻿//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
+//using OnionArchitectureProject.Domain.Authentication.ApplicationRole;
 
-namespace OnionArchitectureProject.Authentication.Configurations;
-public class RoleConfiguration : IEntityTypeConfiguration<ApplicationRole>
-{
-    public void Configure(EntityTypeBuilder<ApplicationRole> builder)
-    {
-        //builder.HasData(
-        //    new IdentityRole
-        //    {
-        //        Id = "9845f909-799c-45fd-9158-58c1336ffddc",
-        //        Name = "User",
-        //        NormalizedName = "USER"
-        //    },
-        //    new IdentityRole
-        //    {
-        //        Id = "cb275765-1cac-4652-a03f-f8871dd575d1",
-        //        Name = "Admin",
-        //        NormalizedName = "ADMIN"
-        //    }
-        //);
-    }
-}
+//namespace OnionArchitectureProject.Authentication.Configurations;
+//public class RoleConfiguration : IEntityTypeConfiguration<ApplicationRole>
+//{
+//    public void Configure(EntityTypeBuilder<ApplicationRole> builder)
+//    {
+//        //builder.HasData(
+//        //    new IdentityRole
+//        //    {
+//        //        Id = "9845f909-799c-45fd-9158-58c1336ffddc",
+//        //        Name = "User",
+//        //        NormalizedName = "USER"
+//        //    },
+//        //    new IdentityRole
+//        //    {
+//        //        Id = "cb275765-1cac-4652-a03f-f8871dd575d1",
+//        //        Name = "Admin",
+//        //        NormalizedName = "ADMIN"
+//        //    }
+//        //);
+//    }
+//}
--- OnionArchitectureProject.Authentication/Configurations/UserConfiguration.cs
+++ OnionArchitectureProject.Authentication/Configurations/UserConfiguration.cs
@@ -1,40 +1,40 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using OnionArchitectureProject.Domain.Authentication;
+﻿//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
+//using OnionArchitectureProject.Domain.Authentication;
 
-namespace OnionArchitectureProject.Authentication.Configurations;
-public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
-{
-    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
-    {
-        //var hasher = new PasswordHasher<ApplicationUser>();
+//namespace OnionArchitectureProject.Authentication.Configurations;
+//public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+//{
+//    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+//    {
+//        //var hasher = new PasswordHasher<ApplicationUser>();
 
-        //builder.HasData(
-        //    new ApplicationUser
-        //    {
-        //        Id = "05446344-f9cc-4566-bd2c-36791b4e28ed",
-        //        Email = "admin@localhost.com",
-        //        NormalizedEmail = "ADMIN@LOCALHOST.COM",
-        //        FirstName = "Admin",
-        //        LastName = "System",
-        //        UserName = "Admin10",
-        //        NormalizedUserName = "ADMIN10",
-        //        PasswordHash = hasher.HashPassword(null, "P@ssword1"),
-        //        EmailConfirmed = true,
-        //    },
-        //    new ApplicationUser
-        //    {
-        //        Id = "2ec9f480-7288-4d0f-a1cd-53cc89968b45",
-        //        Email = "user@localhost.com",
-        //        NormalizedEmail = "USER@LOCALHOST.COM",
-        //        FirstName = "User",
-        //        LastName = "System",
-        //        UserName = "User20",
-        //        NormalizedUserName = "USER20",
-        //        PasswordHash = hasher.HashPassword(null, "P@ssword1"),
-        //        EmailConfirmed = true,
-        //    }
-        //);
-    }
-}
+//        //builder.HasData(
+//        //    new ApplicationUser
+//        //    {
+//        //        Id = "05446344-f9cc-4566-bd2c-36791b4e28ed",
+//        //        Email = "admin@localhost.com",
+//        //        NormalizedEmail = "ADMIN@LOCALHOST.COM",
+//        //        FirstName = "Admin",
+//        //        LastName = "System",
+//        //        UserName = "Admin10",
+//        //        NormalizedUserName = "ADMIN10",
+//        //        PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+//        //        EmailConfirmed = true,
+//        //    },
+//        //    new ApplicationUser
+//        //    {
+//        //        Id = "2ec9f480-7288-4d0f-a1cd-53cc89968b45",
+//        //        Email = "user@localhost.com",
+//        //        NormalizedEmail = "USER@LOCALHOST.COM",
+//        //        FirstName = "User",
+//        //        LastName = "System",
+//        //        UserName = "User20",
+//        //        NormalizedUserName = "USER20",
+//        //        PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+//        //        EmailConfirmed = true,
+//        //    }
+//        //);
+//    }
+//}
